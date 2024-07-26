515ffaac410d09ac6d7851ab89b7e2f08bb3a03c
fabio-gonzalez-itti-digital <fabio.gonzalez@itti.digital>
2024-07-26 16:24:21 -0400

    Implementación de subida de archivos.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import subprocess
 import os
 import json
 import hashlib
+import base64
+from botocore.exceptions import ClientError
 import aws
 
 class bcolors:
@@ -141,7 +143,7 @@ def opt_download(profile: str) -> None:
         for entry in entries:
             print("\n" + bcolors.OKGREEN + "Descargando: " + entry.filename + bcolors.ENDC)
 
-            # Descargar archivo de indice desde secretes manager.
+            # Descargar archivo de indice desde secrets manager.
             indexname = entry.secretname + ".index"
             print("Obteniendo índice: " + indexname)
             value, err = aws.retrieve_secret(indexname, profile)
@@ -188,7 +190,9 @@ def opt_download(profile: str) -> None:
             try:
                 with open(targetfile, "w", encoding="utf-8") as file:
                     for partsbuff_entry in partsbuff:
-                        file.write(partsbuff_entry)
+                        # Decodificar desde base64.
+                        plain = base64.b64decode(partsbuff_entry).decode("utf-8")
+                        file.write(plain)
                         file.flush()
                 print("Escritura: ok.")
             except Exception as e:
@@ -204,7 +208,109 @@ def opt_download(profile: str) -> None:
             else:
                 print("Comprobación: ko.")
 
+def opt_upload(profile):
+    """
+    Procesa las entradas en archivos tipo descriptor y se encarga de subir el contenido de los archivos indicados y asociarlos
+    a un secreto de aws secrets manager.
+    profile: Perfil aws a utilizar.
+    """
+    # Obtener descriptores en carpeta actual.
+    descriptors = get_descriptor_files(".")
+    if (len(descriptors) == 0):
+        print("No se encontraron archivos descriptores.")
+        exit(1)
 
+    # Extraer entradas de cada archivo descriptor.
+    for descriptor in descriptors:
+        entries = parse_descriptor_file(descriptor)
+        for entry in entries:
+            print("\n" + bcolors.OKGREEN + "Subiendo: " + entry.filename + bcolors.ENDC)
+
+            # El archivo debe existir.
+            targetfile = os.path.join(".", entry.filename)
+            if(os.path.exists(targetfile) == False):
+                print(bcolors.FAIL + "Error: el archivo no existe." + bcolors.ENDC)
+                continue
+
+            # Calcular hash md5 de archivo.
+            print("Calculando valor de comprobación ...")
+            hash_md5 = ""
+            try:
+                hash_md5 = hash_file(targetfile)
+            except Exception as e:
+                print("Error: " + str(e))
+                print(bcolors.FAIL + "Error: no se pudo calcular el valor de comprobación." + bcolors.ENDC)
+                continue
+
+            # Codificar contenido en base64 y separar en chunks.
+            # NOTE: Cada chunk contiene 50kb de contenido en base64. El valor
+            # máximo a la fecha para un secret es de 64kb.
+            print("Calculando partes ...")
+            CHUNK_SIZE = 50 * 1024
+            chunks = list()
+            try:
+                with open(targetfile, "rb") as file:
+                    b64 = base64.b64encode(file.read()).decode("utf-8")
+                    for i in range(0, len(b64), CHUNK_SIZE):
+                        chunks.append(b64[i : i + CHUNK_SIZE])
+            except Exception as e:
+                print("Error: " + str(e))
+                print(bcolors.FAIL + "Error: no se pudieron calcular las partes del archivo." + bcolors.ENDC)
+                continue
+
+            # Crear indice.
+            print("Creando archivo índice ...")
+            try:
+                value = {
+                    "v": 1,
+                    "hash": hash_md5,
+                    "parts": len(chunks)
+                }
+                value = json.dumps(value)
+                indexname = entry.secretname + ".index"
+                response, err = aws.update_secret(indexname, value, profile)
+                if err != None and isinstance(err, ClientError):
+                    response, err = aws.create_secret(indexname, value, profile)
+                if err != None:
+                    raise err
+                print("Archivo índice: ok.")
+            except Exception as e:
+                print("Error: " + str(e))
+                print(bcolors.FAIL + "Error: no se puedo crear el archivo índice." + bcolors.ENDC)
+                continue
+
+            # Crear partes.
+            i = 0
+            for chunk in chunks:
+                print("Subiendo parte: {0}/{1}".format(i + 1, len(chunks)))
+                try:
+                    # Crear parte.
+                    partname = entry.secretname + ".{0}".format(i)
+                    response, err = aws.update_secret(partname, chunk, profile)
+                    if err != None and isinstance(err, ClientError):
+                        response, err = aws.create_secret(partname, chunk, profile)
+                    if err != None:
+                        raise err
+                except Exception as e:
+                    print("Error: " + str(e))
+                    print(bcolors.FAIL + "Error: no se puedo subir la parte calculada." + bcolors.ENDC)
+                    continue
+
+                # Siguiente numero de parte.
+                i = i + 1
+
+def resolve_aws_profile(args: any) -> str:
+    """
+    Determina el perfil AWS a utilizar para ciertas operaciones que lo requieren. Imprime advertencias si es necesario.
+    """
+    # Resolver perfil aws. Si no se indica, se utiliza "default".
+    profile = args.aws_profile
+    if profile == None or profile == "":
+        profile = "default"
+        print(bcolors.WARNING + "Advertencia: utilizando perfil por defecto \"default\"." + bcolors.ENDC)
+    else:
+        print("{0}Utilizando perfil \"{1}\".{2}".format(bcolors.OKBLUE, profile, bcolors.ENDC))
+    return profile
 
 def main() -> None:
     """
@@ -220,16 +326,14 @@ def main() -> None:
         opt_check()
 
     if args.action == "download":
-        # Resolver perfil aws. Si no se indica, se utiliza "default".
-        profile = args.aws_profile
-        if profile == None or profile == "":
-            profile = "default"
-            print(bcolors.WARNING +
-                  "Advertencia: utilizando perfil por defecto \"default\"." + bcolors.ENDC)
-        else:
-            print("Utilizando perfil \"{0}\".".format(profile))
         # Operar.
+        profile = resolve_aws_profile(args)
         opt_download(profile)
+
+    if args.action == "upload":
+        # Operar.
+        profile = resolve_aws_profile(args)
+        opt_upload(profile)
 
 
 if __name__ == '__main__':
