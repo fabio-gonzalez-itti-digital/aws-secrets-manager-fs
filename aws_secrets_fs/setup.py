163ff966141ed13134ed776158118bbc2fa61876
fabio-gonzalez-itti-digital <fabio.gonzalez@itti.digital>
2024-10-11 11:31:27 -0300

    bump de versión.

--- aws_secrets_fs/setup.py
+++ aws_secrets_fs/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='aws_secrets_fs',
-    version='0.0.3',
+    version='0.0.4',
     description='Herramienta que permite sincronizar archivos con datos sensibles, utilizando AWS Secrets Manager como backend.',
     url='https://github.com/fabio-gonzalez-itti/aws-secrets-manager-fs',
     author='Fabio Antonio González Sosa',
